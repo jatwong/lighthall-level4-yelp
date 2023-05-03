c911d5f3e3c9f08f88a7cd082abfdfdbbe0bab48
Justina Wong <justinawong97@gmail.com>
2023-05-03 14:04:11 -0700

    add header and navigation

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import WheelOfMeals from "./components/Pages/WheelOfMeals";
 import FindARestaurant from "./components/Pages/FindARestaurant";
+import Header from "./components/UI/Header";
 
 function App() {
   return (
     <>
+    <Header />
       <Routes>
+        <Route path="/" element={ <WheelOfMeals /> } />
         <Route path="/wheel-of-meals" element={<WheelOfMeals />} />
         <Route path="/find-a-restaurant" element={<FindARestaurant />} />
       </Routes>
--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,3 +1,29 @@
-const Header = () => {};
+import { NavLink } from "react-router-dom";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <div className={classes.header}>
+      <NavLink className={classes.logo} to="/wheel-of-meals">
+        lucky<span>bite</span>
+      </NavLink>
+      <nav className={classes["links-group"]}>
+        <NavLink
+          className={({ isActive }) => (isActive ? `${classes.selected}` : "")}
+          to="/wheel-of-meals"
+        >
+          Wheel of Meals
+        </NavLink>
+
+        <NavLink
+          className={({ isActive }) => (isActive ? `${classes.selected}` : "")}
+          to="/find-a-restaurant"
+        >
+          Find a restaurant
+        </NavLink>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
